e2a3baee9adcf6f0bb4e730ed1f80099e6278798
sweet202020 <dolcealessio1@gmail.com>
2022-12-15 15:30:07 +0100

    add dynamic data with compact

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
 <body>
     <header>
         <h1 class="text-uppercase text-center mt-4">hello world</h1>
+        <h3 class="text-center">{{$user}}</h3>
     </header>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
